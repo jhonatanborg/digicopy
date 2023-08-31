f86997c6aee905ab8cacdc9c7b498962534e17b5
jhonatanborg <jhonatanborgesdj@gmail.com>
2023-08-31 16:51:01 -0300

    🚀 RELEASE: create project

---
+++ interno/conexao_bd.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "vedassistemas.com.br";
+$usuario = "vedas213_AdmD";
+$senha = "12345";
+$bd = "vedas213_acesso";
+
+$conn = new mysqli($host, $usuario, $senha, $bd);
+
+if($conn->connect_errno) 
+	echo "Erro ao se conectar: (".$conn->connect_errno.") ".$conn->connect_error;
+	 
+?>
